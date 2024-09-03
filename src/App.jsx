6302244bb1d04bb7ea861d7e54352d2ad7149e6c
Sina <sinaderik7@gmail.com>
2024-09-03 13:44:57 +0330

    feat: reducer function got refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react'
-import reducer from './reducer'
+// import reducer from './reducer'
+import { reducer, ACTION } from './reducer'
 import './styles/app.css'
 
 
@@ -7,14 +8,10 @@ function App() {
 
   const initialState = {
     count: 0,
+    inputText: '',
+    errorMessage:'',
     error: false
   }
-
-  const ACTION = {
-    INCREMENT: "increase",
-    DECREMENT: "decrease",
-  }
-
 
   const [state, dispatch] = useReducer(reducer, initialState)
 
@@ -23,13 +20,16 @@ function App() {
       <div className='counter__wrapper'>
         <h2>Counter: {state.count}</h2>
         <div className='counter__btn-wrapper'>
-          <button onClick={() => dispatch({ type: 'increase' })}>Increase</button>
-          <button onClick={() => dispatch({ type: 'decrease' })}>Decrease</button>
+          <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>Increase</button>
+          <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>Decrease</button>
         </div>
+        {state.error && <h2 className='error__text'>{state.errorMessage}</h2>}
       </div>
       <div className='text__wrapper'>
-        <input type="text" />
-        <h2></h2>
+        <input onChange={(event) => dispatch({ type: ACTION.NEW_INPUT, payload: event.target.value })} type="text" />
+        <h2>{state.inputText}</h2>
+        {/* {state.error?console.log(state.error):''} */}
+        
       </div>
     </div>
   )
--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,28 @@
-function reducer(state, action) {
-    switch (action.type) {
-      case 'increase':
-        return { ...state, count: state.count + 1 }
-      case 'decrease':
-        return { ...state, count: state.count - 1 }
-      default:
-        text = "Looking forward to the Weekend";
-    }
-  }
+const ACTION = {
+  INCREMENT: "increase",
+  DECREMENT: "decrease",
+  NEW_INPUT: "newInput"
+}
 
-export default reducer
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increase':
+      if (state.count < 5) {
+        return { ...state, count: state.count + 1, error: false}
+      } else {
+        return { ...state, error: true, errorMessage: 'Maximum Reached' }
+      }
+    case 'decrease':
+      if (state.count > 0) {
+        return { ...state, count: state.count - 1, error: false}
+      } else {
+        return { ...state, error: true, errorMessage: 'Minimum Reached' }
+      }
+    case 'newInput':
+      return { ...state, inputText: action.payload }
+    default:
+      text = "Looking forward to the Weekend";
+  }
+}
+
+export { reducer, ACTION }
